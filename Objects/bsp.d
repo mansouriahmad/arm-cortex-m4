9630b67d07cd293a655f5b6b4c877f1f98cfa1f4
mansouriahmad <31290993+mansouriahmad@users.noreply.github.com>
2020-04-05 23:58:13 -0400

    Added file via upload

---
+++ Objects/bsp.d
@@ -0,0 +1,11 @@
+.\objects\bsp.o: bsp.c
+.\objects\bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\bsp.o: bsp.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\bsp.o: C:\Users\Ahmad\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
---
+++ Objects/main.d
@@ -0,0 +1,4 @@
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: bsp.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
